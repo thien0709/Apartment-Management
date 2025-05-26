fdd44d181509331d5467a05677dcc0f1cb4ca292
thien0709 <thien070904@gmail.com>
2025-05-26 23:10:12 +0700

    fix : send, receive message admin

--- Apartment-Management-BE/src/main/java/com/apartment_management/controllers/ApiChatController.java
+++ Apartment-Management-BE/src/main/java/com/apartment_management/controllers/ApiChatController.java
@@ -4,7 +4,9 @@ import com.apartment_management.pojo.User;
 import com.apartment_management.services.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,22 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author thien
  */
-
 @RestController
 @RequestMapping("/api")
 public class ApiChatController {
-    
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/admins")
     public List<User> getAllAdmins() {
         return userService.findByRole("ADMIN");
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") int id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
